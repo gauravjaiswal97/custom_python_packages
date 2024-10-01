911141dfacf9a1e30fa6128750cf13cdf5778177
gauravjaiswal97 <gauravjaiswal4499@gmail.com>
2024-10-01 11:57:59 +0530

    custom package testing

---
+++ build/lib/test_trial_ac/__init__.py
@@ -0,0 +1,3 @@
+from .hello_ac import greet
+from .hello_ac import check_palindrome
+from .version import __version__
---
+++ build/lib/test_trial_ac/hello_ac.py
@@ -0,0 +1,5 @@
+def greet():
+    print("Good Luck Everyone!")
+
+def check_palindrome(s):
+    print(s==s[::-1])
